368b854e3b1ce27a2f6369d365a78bd27990005a
AleexxG <alexanderg.sln@gmail.com>
2024-10-20 21:29:37 +0200

    Text input added with button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,43 @@
+import { Colors } from '@/constants/Colors';
+import { Button, StyleSheet, TextInput } from 'react-native';
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Index() {    
+    
+    function onPressLearnMore() {
+        console.log(123);
+    }
+
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text>Home</Text>
+		<View style={styles.container}>
+            <Text style={styles.welcomeText}>Welcome 👋</Text>
+
+            <TextInput 
+                placeholder='Enter your username'
+                style={styles.usernameInput}
+            />
+            <Button
+                onPress={onPressLearnMore}
+                title="Save"
+                color={ Colors.light.text }
+                accessibilityLabel="Save your username"
+            />
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 30,
+    },
+    welcomeText: {
+        fontSize: 30,
+        marginBottom: 50,
+    },
+    usernameInput: {
+        height: 40,
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 15,
+    }
+});
